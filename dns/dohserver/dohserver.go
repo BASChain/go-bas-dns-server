7721128c730ad44fb455c0a5584636945198dc69
stephen.liao <stephen.liao@tutanota.com>
2019-12-20 09:33:45 +0800

    add to repo

--- dns/dohserver/dohserver.go
+++ dns/dohserver/dohserver.go
@@ -1,11 +1,16 @@
 package dohserver
 
 import (
+	"context"
+	"fmt"
 	"github.com/BASChain/go-bas-dns-server/config"
+	"github.com/m13253/dns-over-https/json-dns"
 	"github.com/miekg/dns"
 	"github.com/pkg/errors"
+	"net"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -54,6 +59,11 @@ func NewDohServer() *DohServer {
 
 }
 
+const (
+	VERSION    = "1.1.0"
+	USER_AGENT = "DNS-over-HTTPS/" + VERSION + " (github.com/BASChain/go-bas-dns-server)"
+)
+
 func (doh *DohServer) StartDaemon() error {
 	if doh.dohServer == nil {
 		return errors.New("No Server, Please Init first")
@@ -64,5 +74,196 @@ func (doh *DohServer) StartDaemon() error {
 }
 
 func (doh *DohServer) handlerFunc(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Max-Age", "3600")
+	w.Header().Set("Server", USER_AGENT)
+	w.Header().Set("X-Powered-By", USER_AGENT)
 
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Content-Length", "0")
+		return
+	}
+
+	if r.Form == nil {
+		const maxMemory = 32 << 20 // 32 MB
+		r.ParseMultipartForm(maxMemory)
+	}
+	contentType := r.Header.Get("Content-Type")
+	if ct := r.FormValue("ct"); ct != "" {
+		contentType = ct
+	}
+	if contentType == "" {
+		// Guess request Content-Type based on other parameters
+		if r.FormValue("name") != "" {
+			contentType = "application/dns-json"
+		} else if r.FormValue("dns") != "" {
+			contentType = "application/dns-message"
+		}
+	}
+
+	var responseType string
+	for _, responseCandidate := range strings.Split(r.Header.Get("Accept"), ",") {
+		responseCandidate = strings.SplitN(responseCandidate, ";", 2)[0]
+		if responseCandidate == "application/json" {
+			responseType = "application/json"
+			break
+		} else if responseCandidate == "application/dns-udpwireformat" {
+			responseType = "application/dns-message"
+			break
+		} else if responseCandidate == "application/dns-message" {
+			responseType = "application/dns-message"
+			break
+		}
+	}
+	if responseType == "" {
+		// Guess response Content-Type based on request Content-Type
+		if contentType == "application/dns-json" {
+			responseType = "application/json"
+		} else if contentType == "application/dns-message" {
+			responseType = "application/dns-message"
+		} else if contentType == "application/dns-udpwireformat" {
+			responseType = "application/dns-message"
+		}
+	}
+
+	var req *DNSRequest
+	if contentType == "application/dns-json" {
+		req = doh.parseRequestGoogle(ctx, w, r)
+	} else if contentType == "application/dns-message" {
+		req = doh.parseRequestIETF(ctx, w, r)
+	} else if contentType == "application/dns-udpwireformat" {
+		req = doh.parseRequestIETF(ctx, w, r)
+	} else {
+		jsonDNS.FormatError(w, fmt.Sprintf("Invalid argument value: \"ct\" = %q", contentType), 415)
+		return
+	}
+	if req.errcode == 444 {
+		return
+	}
+	if req.errcode != 0 {
+		jsonDNS.FormatError(w, req.errtext, req.errcode)
+		return
+	}
+
+	req = doh.patchRootRD(req)
+
+	var err error
+	req, err = doh.doDNSQuery(ctx, req)
+	if err != nil {
+		jsonDNS.FormatError(w, fmt.Sprintf("DNS query failure (%s)", err.Error()), 503)
+		return
+	}
+
+	if responseType == "application/json" {
+		doh.generateResponseGoogle(ctx, w, r, req)
+	} else if responseType == "application/dns-message" {
+		doh.generateResponseIETF(ctx, w, r, req)
+	} else {
+		panic("Unknown response Content-Type")
+	}
+
+}
+
+func (doh *DohServer) findClientIP(r *http.Request) net.IP {
+	noEcs := r.URL.Query().Get("no_ecs")
+	if strings.ToLower(noEcs) == "true" {
+		return nil
+	}
+
+	XForwardedFor := r.Header.Get("X-Forwarded-For")
+	if XForwardedFor != "" {
+		for _, addr := range strings.Split(XForwardedFor, ",") {
+			addr = strings.TrimSpace(addr)
+			ip := net.ParseIP(addr)
+			if jsonDNS.IsGlobalIP(ip) {
+				return ip
+			}
+		}
+	}
+	XRealIP := r.Header.Get("X-Real-IP")
+	if XRealIP != "" {
+		addr := strings.TrimSpace(XRealIP)
+		ip := net.ParseIP(addr)
+		if jsonDNS.IsGlobalIP(ip) {
+			return ip
+		}
+	}
+	remoteAddr, err := net.ResolveTCPAddr("tcp", r.RemoteAddr)
+	if err != nil {
+		return nil
+	}
+	if ip := remoteAddr.IP; jsonDNS.IsGlobalIP(ip) {
+		return ip
+	}
+	return nil
+}
+
+func (doh *DohServer) patchRootRD(req *DNSRequest) *DNSRequest {
+	for _, question := range req.request.Question {
+		if question.Name == "." {
+			req.request.RecursionDesired = true
+		}
+	}
+	return req
+}
+
+func (doh *DohServer) indexQuestionType(msg *dns.Msg, qtype uint16) int {
+	for i, question := range msg.Question {
+		if question.Qtype == qtype {
+			return i
+		}
+	}
+	return -1
+}
+
+func (doh *DohServer) doDNSQuery(ctx context.Context, req *DNSRequest) (resp *DNSRequest, err error) {
+
+	// TODO(m13253): Make ctx work. Waiting for a patch for ExchangeContext from miekg/dns.
+	/*
+		numServers := len(s.conf.Upstream)
+		for i := uint(0); i < s.conf.Tries; i++ {
+			req.currentUpstream = s.conf.Upstream[rand.Intn(numServers)]
+
+			upstream, t := addressAndType(req.currentUpstream)
+
+			switch t {
+			default:
+				log.Printf("invalid DNS type %q in upstream %q", t, upstream)
+				return nil, &configError{"invalid DNS type"}
+				// Use DNS-over-TLS (DoT) if configured to do so
+			case "tcp-tls":
+				req.response, _, err = s.tcpClientTLS.Exchange(req.request, upstream)
+			case "tcp", "udp":
+				// Use TCP if always configured to or if the Query type dictates it (AXFR)
+				if t == "tcp" || (s.indexQuestionType(req.request, dns.TypeAXFR) > -1) {
+					req.response, _, err = s.tcpClient.Exchange(req.request, upstream)
+				} else {
+					req.response, _, err = s.udpClient.Exchange(req.request, upstream)
+					if err == nil && req.response != nil && req.response.Truncated {
+						log.Println(err)
+						req.response, _, err = s.tcpClient.Exchange(req.request, upstream)
+					}
+
+					// Retry with TCP if this was an IXFR request and we only received an SOA
+					if (s.indexQuestionType(req.request, dns.TypeIXFR) > -1) &&
+						(len(req.response.Answer) == 1) &&
+						(req.response.Answer[0].Header().Rrtype == dns.TypeSOA) {
+						req.response, _, err = s.tcpClient.Exchange(req.request, upstream)
+					}
+				}
+			}
+
+			if err == nil {
+				return req, nil
+			}
+			log.Printf("DNS error from upstream %s: %s\n", req.currentUpstream, err.Error())
+		}
+		return req, err
+
+	*/
+
+	return
 }
---
+++ dns/dohserver/ietf.go
@@ -0,0 +1,166 @@
+package dohserver
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"github.com/m13253/dns-over-https/json-dns"
+	"github.com/miekg/dns"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func (doh *DohServer) parseRequestIETF(ctx context.Context, w http.ResponseWriter, r *http.Request) *DNSRequest {
+	requestBase64 := r.FormValue("dns")
+	requestBinary, err := base64.RawURLEncoding.DecodeString(requestBase64)
+	if err != nil {
+		return &DNSRequest{
+			errcode: 400,
+			errtext: fmt.Sprintf("Invalid argument value: \"dns\" = %q", requestBase64),
+		}
+	}
+	if len(requestBinary) == 0 && (r.Header.Get("Content-Type") == "application/dns-message" || r.Header.Get("Content-Type") == "application/dns-udpwireformat") {
+		requestBinary, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			return &DNSRequest{
+				errcode: 400,
+				errtext: fmt.Sprintf("Failed to read request body (%s)", err.Error()),
+			}
+		}
+	}
+	if len(requestBinary) == 0 {
+		return &DNSRequest{
+			errcode: 400,
+			errtext: fmt.Sprintf("Invalid argument value: \"dns\""),
+		}
+	}
+
+	if doh.patchDNSCryptProxyReqID(w, r, requestBinary) {
+		return &DNSRequest{
+			errcode: 444,
+		}
+	}
+
+	msg := new(dns.Msg)
+	err = msg.Unpack(requestBinary)
+	if err != nil {
+		return &DNSRequest{
+			errcode: 400,
+			errtext: fmt.Sprintf("DNS packet parse failure (%s)", err.Error()),
+		}
+	}
+
+	transactionID := msg.Id
+	msg.Id = dns.Id()
+	opt := msg.IsEdns0()
+	if opt == nil {
+		opt = new(dns.OPT)
+		opt.Hdr.Name = "."
+		opt.Hdr.Rrtype = dns.TypeOPT
+		opt.SetUDPSize(dns.DefaultMsgSize)
+		opt.SetDo(false)
+		msg.Extra = append([]dns.RR{opt}, msg.Extra...)
+	}
+	var edns0Subnet *dns.EDNS0_SUBNET
+	for _, option := range opt.Option {
+		if option.Option() == dns.EDNS0SUBNET {
+			edns0Subnet = option.(*dns.EDNS0_SUBNET)
+			break
+		}
+	}
+	isTailored := edns0Subnet == nil
+	if edns0Subnet == nil {
+		ednsClientFamily := uint16(0)
+		ednsClientAddress := doh.findClientIP(r)
+		ednsClientNetmask := uint8(255)
+		if ednsClientAddress != nil {
+			if ipv4 := ednsClientAddress.To4(); ipv4 != nil {
+				ednsClientFamily = 1
+				ednsClientAddress = ipv4
+				ednsClientNetmask = 24
+			} else {
+				ednsClientFamily = 2
+				ednsClientNetmask = 56
+			}
+			edns0Subnet = new(dns.EDNS0_SUBNET)
+			edns0Subnet.Code = dns.EDNS0SUBNET
+			edns0Subnet.Family = ednsClientFamily
+			edns0Subnet.SourceNetmask = ednsClientNetmask
+			edns0Subnet.SourceScope = 0
+			edns0Subnet.Address = ednsClientAddress
+			opt.Option = append(opt.Option, edns0Subnet)
+		}
+	}
+
+	return &DNSRequest{
+		request:       msg,
+		transactionID: transactionID,
+		isTailored:    isTailored,
+	}
+}
+
+func (doh *DohServer) generateResponseIETF(ctx context.Context, w http.ResponseWriter, r *http.Request, req *DNSRequest) {
+	respJSON := jsonDNS.Marshal(req.response)
+	req.response.Id = req.transactionID
+	respBytes, err := req.response.Pack()
+	if err != nil {
+		log.Printf("DNS packet construct failure with upstream %s: %v\n", req.currentUpstream, err)
+		jsonDNS.FormatError(w, fmt.Sprintf("DNS packet construct failure (%s)", err.Error()), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/dns-message")
+	now := time.Now().UTC().Format(http.TimeFormat)
+	w.Header().Set("Date", now)
+	w.Header().Set("Last-Modified", now)
+	w.Header().Set("Vary", "Accept")
+
+	_ = doh.patchFirefoxContentType(w, r, req)
+
+	if respJSON.HaveTTL {
+		if req.isTailored {
+			w.Header().Set("Cache-Control", "private, max-age="+strconv.FormatUint(uint64(respJSON.LeastTTL), 10))
+		} else {
+			w.Header().Set("Cache-Control", "public, max-age="+strconv.FormatUint(uint64(respJSON.LeastTTL), 10))
+		}
+		w.Header().Set("Expires", respJSON.EarliestExpires.Format(http.TimeFormat))
+	}
+
+	if respJSON.Status == dns.RcodeServerFailure {
+		log.Printf("received server failure from upstream %s: %v\n", req.currentUpstream, req.response)
+		w.WriteHeader(503)
+	}
+	_, err = w.Write(respBytes)
+	if err != nil {
+		log.Printf("failed to write to client: %v\n", err)
+	}
+}
+
+func (doh *DohServer) patchDNSCryptProxyReqID(w http.ResponseWriter, r *http.Request, requestBinary []byte) bool {
+	if strings.Contains(r.UserAgent(), "dnscrypt-proxy") && bytes.Equal(requestBinary, []byte("\xca\xfe\x01\x00\x00\x01\x00\x00\x00\x00\x00\x01\x00\x00\x02\x00\x01\x00\x00\x29\x10\x00\x00\x00\x80\x00\x00\x00")) {
+		log.Println("DNSCrypt-Proxy detected. Patching response.")
+		w.Header().Set("Content-Type", "application/dns-message")
+		w.Header().Set("Vary", "Accept, User-Agent")
+		now := time.Now().UTC().Format(http.TimeFormat)
+		w.Header().Set("Date", now)
+		w.Write([]byte("\xca\xfe\x81\x05\x00\x01\x00\x01\x00\x00\x00\x00\x00\x00\x02\x00\x01\x00\x00\x10\x00\x01\x00\x00\x00\x00\x00\xa8\xa7\r\nWorkaround a bug causing DNSCrypt-Proxy to expect a response with TransactionID = 0xcafe\r\nRefer to https://github.com/jedisct1/dnscrypt-proxy/issues/526 for details."))
+		return true
+	}
+	return false
+}
+
+func (doh *DohServer) patchFirefoxContentType(w http.ResponseWriter, r *http.Request, req *DNSRequest) bool {
+	if strings.Contains(r.UserAgent(), "Firefox") && strings.Contains(r.Header.Get("Accept"), "application/dns-udpwireformat") && !strings.Contains(r.Header.Get("Accept"), "application/dns-message") {
+		log.Println("Firefox 61-62 detected. Patching response.")
+		w.Header().Set("Content-Type", "application/dns-udpwireformat")
+		w.Header().Set("Vary", "Accept, User-Agent")
+		req.isTailored = true
+		return true
+	}
+	return false
+}
