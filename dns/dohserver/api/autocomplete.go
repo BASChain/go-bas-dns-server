91eb56bae870fa0215a506273a2624ba82ffb78f
stephen.liao <stephen.liao@tutanota.com>
2020-03-07 19:01:29 +0800

    add to repo

--- dns/dohserver/api/autocomplete.go
+++ dns/dohserver/api/autocomplete.go
@@ -1,16 +1,85 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/BASChain/go-bas/DataSync"
+	"strings"
+)
 
 type AutoComplete struct {
 
 }
+
+type AutoCompleteReq struct {
+	Text string `json:"text"`
+}
+
+type AutoCompleteResp struct {
+	State int `json:"state"`
+	Data []string `json:"data"`
+} 
+
+
 
 func NewAutoComplete() *AutoComplete {
 	return &AutoComplete{}
 }
 
 func (ac *AutoComplete)ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	if r.Method != "POST" {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{}")
+		return
+	}
+	var body []byte
+	var err error
 
+	if body, err = ioutil.ReadAll(r.Body); err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{}")
+		return
+	}
+
+	acr:=&AutoCompleteReq{}
+
+	err = json.Unmarshal(body,acr)
+	if err!=nil{
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{}")
+		return
+	}
+
+	searchText := acr.Text
+	var data []string
+
+	for _,r:=range DataSync.Records{
+		if strings.Contains(r.GetName(),searchText){
+			data = append(data,r.GetName())
+		}
+	}
+
+	resp:=&AutoCompleteResp{}
+
+	if len(data)>0{
+		resp.State = 1
+	}else {
+		resp.State = 0
+	}
+
+	resp.Data = data
+
+	var bresp []byte
+
+	bresp,err =json.Marshal(*resp)
+	if err != nil{
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "{}")
+		return
+	}
+	w.WriteHeader(200)
+	w.Write(bresp)
 
 }
