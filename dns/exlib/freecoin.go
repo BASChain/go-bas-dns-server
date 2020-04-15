180479fa7eb4d1a6ba353bf2eddb0fc56b227db1
stephen.liao <stephen.liao@tutanota.com>
2020-04-15 16:25:06 +0800

    add to repo

--- dns/exlib/freecoin.go
+++ dns/exlib/freecoin.go
@@ -4,35 +4,37 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 	"math/big"
+	"github.com/BASChain/go-bas/Transactions"
+	"github.com/BASChain/go-bas-dns-server/dns/mem"
 )
 
 func SendFreeBasByContractWrapper(key *keystore.Key,addr common.Address,amount *big.Int){
-	//mem.Update(addr,mem.BAS,mem.WAITING)
-	//go func() {
-	//	if err:=Transactions.SendFreeBasByContract(key,addr,amount);err!=nil{
-	//		mem.Update(addr,mem.BAS,mem.FAILURE)
-	//	}else {
-	//		mem.Update(addr,mem.BAS,mem.SUCCESS)
-	//	}
-	//}()
+	mem.Update(addr,mem.BAS,mem.WAITING)
+	go func() {
+		if err:=Transactions.SendFreeBasByContract(key,addr,amount);err!=nil{
+			mem.Update(addr,mem.BAS,mem.FAILURE)
+		}else {
+			mem.Update(addr,mem.BAS,mem.SUCCESS)
+		}
+	}()
 }
 
 
 
 func SendFreeEthWrapper(key *keystore.Key,toAddress common.Address,amount *big.Int)  {
-	//mem.Update(toAddress,mem.ETH,mem.WAITING)
-	//go func() {
-	//	if err:=Transactions.SendFreeEth(key,toAddress,amount);err!=nil{
-	//		mem.Update(toAddress,mem.ETH,mem.FAILURE)
-	//	}else{
-	//		mem.Update(toAddress,mem.ETH,mem.SUCCESS)
-	//	}
-	//}()
+	mem.Update(toAddress,mem.ETH,mem.WAITING)
+	go func() {
+		if err:=Transactions.SendFreeEth(key,toAddress,amount);err!=nil{
+			mem.Update(toAddress,mem.ETH,mem.FAILURE)
+		}else{
+			mem.Update(toAddress,mem.ETH,mem.SUCCESS)
+		}
+	}()
 }
 
 func CheckIfApplied(addr common.Address) (bool,error ){
 	var b bool
 	var err error
-	//b,err = Transactions.CheckIfApplied(addr)
+	b,err = Transactions.CheckIfApplied(addr)
 	return b,err
 }
